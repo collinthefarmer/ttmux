058d997f70b29cc820c620affb1c6b0ca28f4b75
Collin Farmer <collinthefarmer@gmail.com>
2026-04-15 04:57:04 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "ttmux - modular tmux configuration framework";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "ttmux";
+          version = "0.1.0";
+
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              ./tmux.conf
+              ./init.tmux.conf
+              ./options.tmux.conf
+              ./bindings.tmux.conf
+              ./programs
+              ./scripts
+            ];
+          };
+
+          buildInputs = [ pkgs.perl ];
+
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/share/ttmux
+            cp tmux.conf init.tmux.conf options.tmux.conf bindings.tmux.conf $out/share/ttmux/
+            cp -r programs $out/share/ttmux/
+            cp -r scripts $out/share/ttmux/
+            chmod +x $out/share/ttmux/scripts/clean-context
+
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Modular tmux configuration framework";
+            platforms = platforms.all;
+          };
+        };
+      });
+
+      overlays.default = final: prev: {
+        ttmux = self.packages.${prev.system}.default;
+      };
+    };
+}
